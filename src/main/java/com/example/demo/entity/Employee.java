8c8c8f53535d61d2258140e63d74f47e3d8602f9
Tharun <Tharun@DESKTOP-EMUTKR9>
2023-09-24 15:04:56 +0530

    Sample project of CURD operation using Spring Boot

---
+++ src/main/java/com/example/demo/entity/Employee.java
@@ -0,0 +1,96 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Employee {
+
+	@Id
+	private int id;
+	
+	private String name; 
+	
+	private long mobile;
+	
+	private String roll;
+	
+	private double salary;
+	
+	
+	
+	public Employee() {
+		
+	}
+
+
+	public Employee(int id, String name, long mobile, String desigination, double salary) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.mobile = mobile;
+		this.roll = roll;
+		this.salary = salary;
+	}
+
+
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public long getMobile() {
+		return mobile;
+	}
+
+
+	public void setMobileno(int mobile) {
+		this.mobile = mobile;
+	}
+
+
+	public String getRoll() {
+		return roll;
+	}
+
+
+	public void setRoll(String roll) {
+		this.roll = roll;
+	}
+
+
+	public double getSalary() {
+		return salary;
+	}
+
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", mobile=" + mobile + ", roll=" + roll
+				+ ", salary=" + salary + "]";
+	}
+	
+	
+	
+	
+}
