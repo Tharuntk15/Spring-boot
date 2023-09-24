7697f3edad0469c5095dca75627121f4852be567
Tharun <Tharun@DESKTOP-EMUTKR9>
2023-09-25 00:44:29 +0530

    Sample project of CURD operation

--- src/main/java/com/example/demo/controller/EmployeeController.java
+++ src/main/java/com/example/demo/controller/EmployeeController.java
@@ -3,56 +3,55 @@ package com.example.demo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.demo.entity.Employee;
-import com.example.demo.repository.EmployeeRepo;
+import com.example.demo.entity.EmployeeDTO;
+import com.example.demo.entity.EmployeeUpdateDTO;
+import com.example.demo.service.EmployeeService;
 
 @RestController
+@CrossOrigin
+@RequestMapping("/")
 public class EmployeeController {
 	
 	@Autowired
-	private EmployeeRepo employeerepo;
+	private EmployeeService employeeservice;
 	
 	//Handler for creating data in DB
 	@PostMapping("/save")
-	public Employee saveData(@RequestBody Employee employee) {
-		employeerepo.save(employee);
-		return employee;
+	public String addData(@RequestBody EmployeeDTO employeedto) {
+		String id=employeeservice.adddata(employeedto);
+		return id;
 	}
-	//Handler for fetch single data from DB
-	@GetMapping("/getData/{id}")
-	public Employee getData(@PathVariable int id) {
-		Employee employee= employeerepo.findById(id).get();
-		return employee;
-	}
+	
 	//Handler for fetch the all data from DB
 	@GetMapping("/getAllEmployee")
-	public List<Employee>getAll(){
-		List<Employee> employeeList= employeerepo.findAll();
-		return employeeList;
+	public List<EmployeeDTO>getAll(){
+		List<EmployeeDTO> allEmployees= employeeservice.getAllEmployee();
+		return allEmployees;
+	}
+	//Handler for update data to DB
+	@PutMapping("/updateData")
+	public EmployeeUpdateDTO updateData(@RequestBody EmployeeUpdateDTO employeeupdatedto) {
+		employeeservice.updateData(employeeupdatedto);
+		return employeeupdatedto;
 	}
 	//Handler for delete data from DB
 	@DeleteMapping("/Delete/{id}")
 	public String deleteData(@PathVariable int id) {
-		Employee empolyee=employeerepo.findById(id).get();
-		if(empolyee!=null) 
-			employeerepo.delete(empolyee);
+		boolean deletedata=employeeservice.deleteData(id);
 		return "Deleted Successfully";
 	}
 	
-	@PutMapping("/updateData")
-	public Employee updateData(@RequestBody Employee employee) {
-		employeerepo.save(employee);
-		return employee;
-	}
+	
 	
 
 }
--- src/main/java/com/example/demo/repository/EmployeeRepo.java
+++ src/main/java/com/example/demo/repository/EmployeeRepo.java
@@ -1,10 +1,12 @@
 package com.example.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.entity.Employee;
 
+@EnableJpaRepositories
 @Repository
 public interface EmployeeRepo extends JpaRepository<Employee,Integer> {
 
